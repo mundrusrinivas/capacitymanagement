0a02265abee40d02063824eed9abe07e0bc0a4a8
raghunedunuri <raghu.nedunuri@gmail.com>
2018-01-29 14:52:03 +0530

    Monthly dashboard

--- Scripts/chartjs-custom.js
+++ Scripts/chartjs-custom.js
@@ -150,12 +150,12 @@ $(document).ready(function () {
 
     };
     //new Chart(document.getElementById("doughnut").getContext("2d")).Doughnut(doughnutData);
-    new Chart(document.getElementById("line").getContext("2d")).Line(lineChartData);
+   // new Chart(document.getElementById("line").getContext("2d")).Line(lineChartData);
     //new Chart(document.getElementById("radar").getContext("2d")).Radar(radarChartData);
     //  new Chart(document.getElementById("polarArea").getContext("2d")).PolarArea(chartData);
-    new Chart(document.getElementById("bar").getContext("2d")).Bar(barChartData);
-    new Chart(document.getElementById("bar1").getContext("2d")).Bar(bar1ChartData);
-    new Chart(document.getElementById("pie").getContext("2d")).Pie(pieData);
+    //new Chart(document.getElementById("bar").getContext("2d")).Bar(barChartData);
+    //new Chart(document.getElementById("bar1").getContext("2d")).Bar(bar1ChartData);
+    //new Chart(document.getElementById("pie").getContext("2d")).Pie(pieData);
 
 
 });
